f5b56ddee86d6972b7b073336b46f9e8866e92cf
Isaac Kwemboi Tegei <isaac.kwemboi@student.moringaschool.com>
2022-11-03 20:53:11 +0300

    quizboard

--- js/quizboard.js
+++ js/quizboard.js
@@ -1,32 +1,11 @@
-//document.getElementById("btn-one").innerHTML = "";
-$(document).ready(function (){
-    $(window).resize(function(){
-        $(" .my-btns").css("display", "block")
-    });
-
-    $("#btn-one").click(function (){
-        alert("Doing something");
-    });
-    
-    $("#p-one").click(function (){
-        //alert("Doing something - p1");
-        //document.getElementById("p-one").innerHTML = "Updated"
-        $("#p-one").text("Simulated");
-    });
-    
-    $(".p-two").click(function (){
-        $("Doing something - p2");
-    });
-
-    $("#img").hover(function (){
-        $(this).mouseenter(function(){
-            $("#img-span").show();
-        });
-
-        $(this).mouseleave(function(){
-            $("#img-span").hide();
-        });
-    });
+const quizAnswers = document.querySelectorAll(".ouranswers");
+const submitAnswer = document.querySelector("#submit_ans")
+let marks = 0;
+quizAnswers.forEach(function(jibu){
+   jibu.addEventListener("change",function(event){
+    marks += parseInt(event.target.value);
+   });
 });
-
-$(selector).userAction(callBack);
+submitAnswer.addEventListener("click", function (event) {
+    alert("your score is: " + marks);
+})
